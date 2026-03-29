a752e1fdedebefe6682e0c7635cac9e6d67d8b78
BrickEater <codyratcliffep@gmail.com>
2026-03-29 15:08:27 -0600

    added d_name retrieval from dirent

--- main.c
+++ main.c
@@ -1,76 +1,30 @@
-#include <linux/limits.h>
+// I'm doing this in the wrong order. I've been ignoring that I need to retrieve the path from the user
+// I should really smooth out that function before I do all this data retrieval
+// I should also consider using both stat and lstat. I feel like they return different data for symlinks
 #include <stdio.h>
+#include <sys/types.h>
 #include <dirent.h>
-#include <unistd.h>
 #include <string.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <limits.h>
 
 #include "third_party/cjson/cJSON.h"
-
-cJSON *read_dir(const char *path);
 
 int main()
 {
 
-    char resolved[PATH_MAX];
-    realpath(".", resolved);
-
-    cJSON *entries = read_dir(".");
-    cJSON *root = cJSON_CreateObject();
-    cJSON_AddItemToObject(root, resolved, entries);
-
-    char *output = cJSON_Print(root);
-    printf("%s\n", output);
-
-    free(output);
-    cJSON_Delete(root);
-
-    return(0);
-}
-
-cJSON *read_dir(const char *path)
-{
-    DIR *d = opendir(path);
-    if (!d) return NULL;
-    
-    cJSON *entries = cJSON_CreateArray();
+    DIR *d = opendir(".");
+    if (!d) return 1;
 
     struct dirent *entry;
-    while ((entry = readdir(d)) != NULL)
-    {
+
+    while((entry = readdir(d)) != NULL)
         if(!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
         {
             continue;
         }
-        cJSON *obj = cJSON_CreateObject();
-        cJSON_AddStringToObject(obj, "name", entry->d_name);
-
-        struct stat sb;
-        if (lstat(entry->d_name, &sb) == -1)
+        else
         {
-            perror("lstat");
-            exit(EXIT_FAILURE);
+            printf("%s\n", entry->d_name);
         }
-        cJSON_AddNumberToObject(obj, "inode", sb.st_ino);
-        cJSON_AddNumberToObject(obj, "mode", sb.st_mode);
-        cJSON_AddNumberToObject(obj, "link count", sb.st_nlink);
-        cJSON_AddNumberToObject(obj, "ownership(UID)", sb.st_uid);
-        cJSON_AddNumberToObject(obj, "ownership(GID)", sb.st_gid);
-        cJSON_AddNumberToObject(obj, "preferred IO block size", sb.st_blksize);
-        cJSON_AddNumberToObject(obj, "file size", sb.st_size);
-        cJSON_AddNumberToObject(obj, "blocks allocated", sb.st_blocks);
-        cJSON_AddNumberToObject(obj, "last status change", sb.st_ctime);
-        cJSON_AddNumberToObject(obj, "last file access", sb.st_atime);
-        cJSON_AddNumberToObject(obj, "modification", sb.st_mtime);
 
-        cJSON_AddItemToArray(entries,  obj);
-    }
-
-
-    closedir(d);
-    return entries;
+    return(0);
 }
-
-
